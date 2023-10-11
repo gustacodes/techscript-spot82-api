7723b4cab09c39928025445d55da40ced9a2c58e
Gustavo Alencar <gustavorique@live.com>
2023-10-11 01:33:52 -0300

    Commit inicial

---
+++ src/main/java/com/techscript/spot82/Spot82Application.java
@@ -0,0 +1,13 @@
+package com.techscript.spot82;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spot82Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spot82Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/techscript/spot82/Spot82ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techscript.spot82;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spot82ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
