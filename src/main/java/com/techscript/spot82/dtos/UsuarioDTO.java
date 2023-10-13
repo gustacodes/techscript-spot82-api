e27db5e25e1e3bd9e3a55d148012606cb8cc8d1a
Gustavo Alencar <gustavorique@live.com>
2023-10-13 00:35:15 -0300

    feat: classe dto modificada

--- src/main/java/com/techscript/spot82/dtos/UsuarioDTO.java
+++ src/main/java/com/techscript/spot82/dtos/UsuarioDTO.java
@@ -10,7 +10,7 @@ public class UsuarioDTO {
     private Long id;
     private String nome;
     private String email;
-    private Papel usuario;
+    private Papel papel;
 
     public UsuarioDTO() {
 
@@ -20,6 +20,6 @@ public class UsuarioDTO {
         this.id = usuario.getId();
         this.nome = usuario.getNome();
         this.email = usuario.getEmail();
-        this.usuario = usuario.getPapel();
+        this.papel = usuario.getPapel();
     }
 }
